cfaa3a92096f40c3e83b1615275d62f5455cc770
ahmetcetinkaya <4hmetcetinkaya@gmail.com>
2020-11-04 00:25:45 +0300

    SignIn method added

--- NetCoreBasicApi/Controllers/v1/AuthenticationController.cs
+++ NetCoreBasicApi/Controllers/v1/AuthenticationController.cs
@@ -6,12 +6,21 @@ namespace NetCoreBasicApi.Controllers.v1
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class LoginController : ControllerBase
+    public class AuthenticationController : ControllerBase
     {
         [HttpPost]
+        [Route("LogIn")]
         public LoginResponse Login(LoginRequest loginRequest) 
         {
             LoginResponse result = new LoginResponse();
+            result.IsSuccessful = true;
+            return result;
+        }
+
+        [HttpPost]
+        [Route("SignIn")]
+        public SignInResponse SignIn(SignInRequest signInRequest) {
+            SignInResponse result = new SignInResponse();
             result.IsSuccessful = true;
             return result;
         }
---
+++ NetCoreBasicApi/Model/Contracts/SignInResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NetCoreBasicApi.Model.Contracts
+{
+    public class SignInResponse
+    {
+        public SignInResponse()
+        {
+        }
+
+        public bool IsSuccessful { get; set; }
+    }
+}
